fddb86e98272681dc23ea9bbc8dcd7c986879333
Júlia Wauters <juliawauters04@gmail.com>
2026-02-15 12:45:23 -0300

    feat(feature/TCC-5): Created procedure controller and service

---
+++ app/Services/ProcedureService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Procedure;
+
+class ProcedureService
+{
+    public function all()
+    {
+        return Procedure::orderBy('name')
+            ->with('specialty:id,name')
+            ->get();
+    }
+
+    public function create(array $data, int $universityId): Procedure
+    {
+        if (!$universityId) {
+            throw new \RuntimeException('Salvamento inválido');
+        }
+
+        $procedure = Procedure::create([
+            'name' => $data['name'],
+            'specialty_id' => $data['specialty_id'],
+            'university_id' => $universityId,
+        ]);
+
+        return $procedure->load('specialty:id,name');
+    }
+
+    public function update(Procedure $procedure, array $data): Procedure
+    {
+        $procedure->update([
+            'name' => $data['name'],
+            'specialty_id' => $data['specialty_id'],
+        ]);
+
+        return $procedure->load('specialty');
+    }
+
+    public function delete(Procedure $procedure): void
+    {
+        $procedure->delete();
+    }
+}
