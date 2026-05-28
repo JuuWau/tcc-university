5a802bca233fa4ef383d5dfa57fb65dcd4389af3
Júlia Wauters <juliawauters04@gmail.com>
2026-05-28 10:34:43 -0300

    feat(feature/TCC-16): Implemented new import button

---
+++ resources/js/components/buttons/ImportExcelButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Button from '../ui/button/Button.vue';
+import { Upload } from 'lucide-vue-next';
+
+defineEmits<{
+    click: [];
+}>();
+</script>
+
+<template>
+    <Button
+        type="button"
+        class="border border-green-500 bg-green-500 text-white hover:bg-green-600"
+        @click="$emit('click')"
+    >
+        <Upload class="mr-2 h-4 w-4" />
+
+        Importar Excel
+    </Button>
+</template>
