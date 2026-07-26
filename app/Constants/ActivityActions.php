0e4ab2cfae3351d5dcaee24f1f95f4ac64c2407e
Júlia Wauters <juliawauters04@gmail.com>
2026-07-26 17:21:06 -0300

    feat(feature/TCC-33): created log constants

---
+++ app/Constants/ActivityActions.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class ActivityActions
+{
+    public const CREATE = 'create';
+    public const UPDATE = 'update';
+    public const DELETE = 'delete';
+}
---
+++ app/Constants/ActivityModules.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Constants;
+
+class ActivityModules
+{
+    public const SPECIALTIES = 'Especialidades';
+    public const PERIODS = 'Períodos';
+    public const CLINICS = 'Clínicas';
+    public const PATIENTS = 'Pacientes';
+    public const USERS = 'Usuários';
+    public const PROCEDURES = 'Procedimentos';
+    public const STUDENTS = 'Estudantes';
+    public const SCHEDULES = 'Agendas';
+}
