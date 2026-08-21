8fc276d5b41c59e856f59bbd0426e1aaa9936303
Júlia Wauters <juliawauters04@gmail.com>
2026-08-21 15:15:57 -0300

    feat(feature/TCC-18): New requests

---
+++ app/Http/Requests/PatientScheduleStudentsRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PatientScheduleStudentsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'clinic_id' => [
+                'required',
+                'integer',
+                'exists:clinics,id'
+            ],
+            'period_id' => [
+                'required',
+                'integer',
+                'exists:periods,id'
+            ],
+        ];
+    }
+}
