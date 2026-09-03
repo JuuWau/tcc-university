0356b47a0c1660238cec5fdfe1cb197c1328e882
Júlia Wauters <juliawauters04@gmail.com>
2026-09-03 16:20:36 -0300

    feat(feature/TCC-25): app.ts and app.php changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -27,5 +27,20 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\HttpException $exception, $request) {
+            if ($exception->getStatusCode() !== 403) {
+                return null;
+            }
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Você não possui permissão para realizar esta ação.',
+                ], 403);
+            }
+
+            return redirect()->route('initialPage')->with(
+                'error',
+                'Você não possui permissão para realizar esta ação.'
+            );
+        });
     })->create();
