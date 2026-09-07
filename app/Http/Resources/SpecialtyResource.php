f6ac47cb01bea9397b151679affbd0df8f652ac6
Júlia Wauters <juliawauters04@gmail.com>
2026-09-07 13:32:52 -0300

    feat(feature/TCC-44): new resources

--- app/Http/Resources/SpecialtyResource.php
+++ app/Http/Resources/SpecialtyResource.php
@@ -2,24 +2,21 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class AttendanceDateResource extends JsonResource
+class SpecialtyResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray($request): array
+    public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
-            'date' => $this->date,
-            'label' => $this->date->format('d/m/Y'),
-            'editable' => auth()->user()->hasRole(Role::ADMIN) || $this->date->isToday(),
+            'name' => $this->name,
         ];
     }
 }
