ae3dbd06b2e5158cc38bf8dac309e354c87d5ed1
Júlia Wauters <juliawauters04@gmail.com>
2026-09-08 09:44:09 -0300

    feat(main): initial page changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,7 +31,7 @@
         }
     </style>
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Acadent') }}</title>
 
     <link rel="icon" href="/images/favicon.png" sizes="any">
 
